dbb584f11d2ed2e1d11884d60a5f6d579f618157
New Author Name~ <jinagagiribabu@outlook.com>
2026-01-23 21:00:18 +0530

    terraform-variables

---
+++ conditions/ec2.tf
@@ -0,0 +1,35 @@
+resource "aws_instance" "expense" {
+
+
+  ami                    = var.ami_id
+  instance_type          = var.environment=="prod"? "t3.small" : "t3.micro"
+  vpc_security_group_ids = [aws_security_group.example.id]
+  tags = var.ec2_tags
+}
+
+
+resource "aws_security_group" "example" {
+  name = "giri-sg"
+  #vpc_id = aws_vpc.example.id
+
+  ingress {
+
+    from_port   = var.from_port
+    to_port     = var.to_port
+    protocol    = "tcp"
+    cidr_blocks = var.cidr_blocks
+  }
+
+  egress {
+
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  tags =var.sg-tags
+
+  }
+
